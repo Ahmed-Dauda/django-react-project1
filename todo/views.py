633ff9f4b4e011591c332369401dc6f84d48b42f
Ahmed-Dauda <addatainsight@gmail.com>
2022-09-15 12:05:55 +0100

    First commit adding files

--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 from .serializers import TodoSerializer 
 from rest_framework import viewsets      
 from .models import Todo                 
+import os
+from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound
 
 class TodoView(viewsets.ModelViewSet):  
     serializer_class = TodoSerializer   
     queryset = Todo.objects.all() 
+    
+
+# Add this CBV
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()  
